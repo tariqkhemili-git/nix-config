3baae85154f39e7f864364ebff40655627819ea1
Tariq Khemili <240774599+tariqkhemili-git@users.noreply.github.com>
2026-03-13 17:12:38 +0000

    Updating Garbage Collection

--- modules/system-modules/nix.nix
+++ modules/system-modules/nix.nix
@@ -7,11 +7,13 @@
         "nix-command"
         "flakes"
       ];
+      auto-optimise-store = true;
     };
     gc = {
       automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
+      dates = "daily";
+      options = "--delete-older-than 3d";
+      persistent = true;
     };
   };
 }
