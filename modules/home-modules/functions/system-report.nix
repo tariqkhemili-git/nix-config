b7393d3fc59ecb8d4483e35b8e0b301d6e2c19b4
Tariq Khemili <240774599+tariqkhemili-git@users.noreply.github.com>
2026-03-14 17:21:41 +0000

    Change bash scripts to nix scripts

--- modules/home-modules/functions/system-report.nix
+++ modules/home-modules/functions/system-report.nix
@@ -1,1 +1,27 @@
+{ pkgs, ... }:
 
+{
+  home.packages = [
+    (pkgs.writeShellScriptBin "system-report" ''
+      # Inject hardware utilities safely into the script's PATH
+      export PATH="${
+        pkgs.lib.makeBinPath [
+          pkgs.util-linux
+          pkgs.lm_sensors
+          pkgs.pciutils
+          pkgs.coreutils
+        ]
+      }:$PATH"
+
+      # --- Paste your system-report.sh bash logic below ---
+      # IMPORTANT FIX: Change 'nix run nixpkgs#lsblk' to simply 'lsblk'
+      # IMPORTANT FIX: Change 'nix run nixpkgs#sensors' to simply 'sensors'
+
+      echo "=== Storage Devices ==="
+      lsblk
+
+      echo -e "\n=== Hardware Temperatures ==="
+      sensors
+    '')
+  ];
+}
