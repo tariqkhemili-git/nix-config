6c85a0b7f19dff58efcea9d55d7fb605fe06c2f1
Tariq Khemili <240774599+tariqkhemili-git@users.noreply.github.com>
2026-03-15 11:39:22 +0000

    Nix function for copying, moving, and merging in functions - fix

--- home-modules/functions/cmm.nix
+++ home-modules/functions/cmm.nix
@@ -1,66 +1,70 @@
-home.packages = with pkgs; [
-  # 1. pcopy: Multi-threaded directory copy
-  (writeShellScriptBin "pcopy" ''
-    if [ "$#" -lt 2 ]; then
-      echo "Usage: pcopy <source_directory> <destination_directory>"
-      exit 1
-    fi
+{ ... }:
 
-    SRC=$(${coreutils}/bin/realpath "$1")
-    DST=$(${coreutils}/bin/realpath "$2")
+{
+  home.packages = with pkgs; [
+    # 1. pcopy: Multi-threaded directory copy
+    (writeShellScriptBin "pcopy" ''
+      if [ "$#" -lt 2 ]; then
+        echo "Usage: pcopy <source_directory> <destination_directory>"
+        exit 1
+      fi
 
-    if [ ! -d "$SRC" ]; then
-      echo "Error: Source must be a directory."
-      exit 1
-    fi
+      SRC=$(${coreutils}/bin/realpath "$1")
+      DST=$(${coreutils}/bin/realpath "$2")
 
-    mkdir -p "$DST"
-    echo "Parallel Copy: $SRC -> $DST (20 threads)"
-    ${fpart}/bin/fpsync -n 20 -o '-lptgoDWq' "$SRC/" "$DST/"
-  '')
+      if [ ! -d "$SRC" ]; then
+        echo "Error: Source must be a directory."
+        exit 1
+      fi
 
-  # 2. pmove: Multi-threaded directory move (copy then delete source)
-  (writeShellScriptBin "pmove" ''
-    if [ "$#" -lt 2 ]; then
-      echo "Usage: pmove <source_directory> <destination_directory>"
-      exit 1
-    fi
+      mkdir -p "$DST"
+      echo "Parallel Copy: $SRC -> $DST (20 threads)"
+      ${fpart}/bin/fpsync -n 20 -o '-lptgoDWq' "$SRC/" "$DST/"
+    '')
 
-    SRC=$(${coreutils}/bin/realpath "$1")
-    DST=$(${coreutils}/bin/realpath "$2")
+    # 2. pmove: Multi-threaded directory move (copy then delete source)
+    (writeShellScriptBin "pmove" ''
+      if [ "$#" -lt 2 ]; then
+        echo "Usage: pmove <source_directory> <destination_directory>"
+        exit 1
+      fi
 
-    if [ ! -d "$SRC" ]; then
-      echo "Error: Source must be a directory."
-      exit 1
-    fi
+      SRC=$(${coreutils}/bin/realpath "$1")
+      DST=$(${coreutils}/bin/realpath "$2")
 
-    mkdir -p "$DST"
-    echo "Parallel Move: $SRC -> $DST (20 threads)"
-    ${fpart}/bin/fpsync -n 20 -o '-lptgoDWq --remove-source-files' "$SRC/" "$DST/"
-    
-    # Sweep up empty source directories after successful transfer
-    if [ $? -eq 0 ]; then
-      find "$SRC" -type d -empty -delete
-    fi
-  '')
+      if [ ! -d "$SRC" ]; then
+        echo "Error: Source must be a directory."
+        exit 1
+      fi
 
-  # 3. pmerge: Multi-threaded directory update (only newer files)
-  (writeShellScriptBin "pmerge" ''
-    if [ "$#" -lt 2 ]; then
-      echo "Usage: pmerge <source_directory> <destination_directory>"
-      exit 1
-    fi
+      mkdir -p "$DST"
+      echo "Parallel Move: $SRC -> $DST (20 threads)"
+      ${fpart}/bin/fpsync -n 20 -o '-lptgoDWq --remove-source-files' "$SRC/" "$DST/"
 
-    SRC=$(${coreutils}/bin/realpath "$1")
-    DST=$(${coreutils}/bin/realpath "$2")
+      # Sweep up empty source directories after successful transfer
+      if [ $? -eq 0 ]; then
+        find "$SRC" -type d -empty -delete
+      fi
+    '')
 
-    if [ ! -d "$SRC" ]; then
-      echo "Error: Source must be a directory."
-      exit 1
-    fi
+    # 3. pmerge: Multi-threaded directory update (only newer files)
+    (writeShellScriptBin "pmerge" ''
+      if [ "$#" -lt 2 ]; then
+        echo "Usage: pmerge <source_directory> <destination_directory>"
+        exit 1
+      fi
 
-    mkdir -p "$DST"
-    echo "Parallel Merge: $SRC -> $DST (20 threads)"
-    ${fpart}/bin/fpsync -n 20 -o '-lptgoDWqu' "$SRC/" "$DST/"
-  '')
-];
+      SRC=$(${coreutils}/bin/realpath "$1")
+      DST=$(${coreutils}/bin/realpath "$2")
+
+      if [ ! -d "$SRC" ]; then
+        echo "Error: Source must be a directory."
+        exit 1
+      fi
+
+      mkdir -p "$DST"
+      echo "Parallel Merge: $SRC -> $DST (20 threads)"
+      ${fpart}/bin/fpsync -n 20 -o '-lptgoDWqu' "$SRC/" "$DST/"
+    '')
+  ];
+}
